ce373e951634086d1e352b66e50f5fab1bf5902a
Galovach Andrey <a.galovach@yandex.ru>
2022-08-15 11:08:36 +0300

    Добавил utils для сервиса - получение хешированного ключа

--- src/services/utils.py
+++ src/services/utils.py
@@ -5,10 +5,10 @@ def create_key(params: str) -> str:
     """Получение хешированного ключа для Redis.
 
     Args:
-            params: Параметры запроса к API.
+        params: Параметры запроса к API.
 
-        Returns:
-            str: Список объектов модели Film | None.
+    Returns:
+        str: Хешированный ключ для Redis.
     """
 
     return md5(params.encode()).hexdigest()  # noqa: DUO130
