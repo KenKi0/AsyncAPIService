a2daefe6bdf74edc2ab00c4d37b8585e28c9750f
Galovach Andrey <a.galovach@yandex.ru>
2022-08-15 20:04:28 +0300

    Добавил api для фильмов

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -1,53 +1,80 @@
 from http import HTTPStatus
+from typing import Optional
+from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 
 from models.film import DetailFilmResponse, FilmResponse
-from models.genre import DetailGenre
-from models.person import FilmPerson
 from src.services.film import FilmService, get_film_service
+
+from .utils import get_search
 
 router = APIRouter()
 
 
-# Параметры для запроса:
-# sort
-# page[size] # noqa: E800
-# page[number] # noqa: E800
-# filter[genre] # noqa: E800
 @router.get('/', response_model=FilmResponse)
-async def film_response(*params, film_service: FilmService = Depends(get_film_service)) -> FilmResponse:
+async def film_response(
+    sort: str,
+    film_service: FilmService = Depends(get_film_service),
+    page_num: int = Query(default=1, alias='page[number]'),
+    page_size: int = Query(default=50, alias='page[size]'),
+    _filter: Optional[UUID] = Query(default=None, alias='filter[genre]'),
+) -> Optional[list[FilmResponse]]:
     """Главная страница с фильмами.
 
     Args:
-            *params: .... !!!надо определиться с ними!!!
-            film_service: Провайдер для FilmService.
+        sort: Параметр сортировки.
+        page_num: Номер страницы.
+        page_size: Размер страницы.
+        _filter: Параметр фильтрации по жанрам.
+        film_service: Провайдер для FilmService.
 
     Returns:
-            FilmResponse: Объект модели FilmResponse.
+        Optional[list[FilmResponse]]: Список объектов модели FilmResponse.
     """
+    index = 'movies'
+    search = get_search(
+        sort=sort,
+        page_num=page_num,
+        page_size=page_size,
+        _filter=_filter,
+        index=index,
+    )
+    films = await film_service.get_by_search(search=search)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+    return films
 
-    return
 
-
-# Параметры для запроса:
-# sort ??
-# page[size] # noqa: E800
-# page[number] # noqa: E800
-# query
 @router.get('/search', response_model=FilmResponse)
-async def search_film_response(*params, film_service: FilmService = Depends(get_film_service)) -> FilmResponse:
+async def search_film_response(
+    query: str,
+    film_service: FilmService = Depends(get_film_service),
+    page_num: int = Query(default=1, alias='page[number]'),
+    page_size: int = Query(default=50, alias='page[size]'),
+) -> Optional[list[FilmResponse]]:
     """Поиск по фильмам.
 
     Args:
-            *params: .... !!!надо определиться с ними!!!
-            film_service: Провайдер для FilmService.
+        query: Параметр поиска.
+        page_num: Номер страницы.
+        page_size: Размер страницы.
+        film_service: Провайдер для FilmService.
 
     Returns:
-            FilmResponse: Объект модели FilmResponse.
+        Optional[list[FilmResponse]]: Список объектов модели FilmResponse.
     """
-
-    return
+    index = 'movies'
+    search = get_search(
+        query=query,
+        page_num=page_num,
+        page_size=page_size,
+        index=index,
+    )
+    films = await film_service.get_by_search(search=search)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+    return films
 
 
 @router.get('/{film_id}', response_model=DetailFilmResponse)
@@ -55,27 +82,14 @@ async def film_details(film_id: str, film_service: FilmService = Depends(get_fil
     """Полная информация о фильме.
 
     Args:
-            film_id: id фильма.
-            film_service: Провайдер для FilmService.
+        film_id: id фильма.
+        film_service: Провайдер для FilmService.
 
     Returns:
-            DetailFilmResponse: Объект модели DetailFilmResponse.
+        DetailFilmResponse: Объект модели DetailFilmResponse.
     """
 
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
-    genre_list = [DetailGenre(uuid=item.get('uuid'), name=item.get('name')) for item in film.genre]
-    actors_list = [FilmPerson(uuid=item.get('uuid'), full_name=item.get('full_name')) for item in film.actors]
-    writers_list = [FilmPerson(uuid=item.get('uuid'), full_name=item.get('full_name')) for item in film.writers]
-    directors_list = [FilmPerson(uuid=item.get('uuid'), full_name=item.get('full_name')) for item in film.directors]
-    return DetailFilmResponse(
-        uuid=film.id,
-        title=film.title,
-        imdb_rating=film.imdb_rating,
-        description=film.description,
-        genre=genre_list,
-        actors=actors_list,
-        writers=writers_list,
-        directors=directors_list,
-    )
+    return film
