d56229752be21e599f67503c1973ba97a5cad182
Galovach Andrey <a.galovach@yandex.ru>
2022-08-15 11:06:40 +0300

    Добавил сервис для фильмов

--- src/services/film.py
+++ src/services/film.py
@@ -1,13 +1,17 @@
 from functools import lru_cache
 from typing import Optional
 
+import orjson
 from aioredis import Redis
 from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch_dsl import Search
 from fastapi import Depends
 
 from src.db.elastic import get_elastic
 from src.db.redis import get_redis
-from src.models.film import Film
+from src.models.film import DetailFilmResponse, Film, FilmResponse
+
+from .utils import create_key
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
@@ -23,7 +27,7 @@ class FilmService:
         self.redis = redis
         self.elastic = elastic
 
-    async def get_by_id(self, film_id: str) -> Optional[Film]:
+    async def get_by_id(self, film_id: str) -> Optional[DetailFilmResponse]:
         """Получение и запись информации о фильме.
 
         Args:
@@ -35,12 +39,47 @@ class FilmService:
 
         film = await self._film_from_cache(film_id)
         if not film:
-            film = await self._get_film_from_elastic(film_id)
-            if not film:
+            try:
+                data = await self._get_film_from_elastic(film_id)
+                film = DetailFilmResponse(
+                    uuid=data.id,
+                    title=data.title,
+                    imdb_rating=data.imdb_rating,
+                    description=data.description,
+                    genre=data.genre,
+                    actors=data.actors,
+                    writers=data.writers,
+                    directors=data.director,
+                )
+            except NotFoundError as ex:  # noqa: F841
+                #  TODO logging
                 return None
             await self._put_film_to_cache(film)
-
         return film
+
+    async def get_by_search(self, search: Search) -> Optional[list[FilmResponse]]:
+        """
+        Получение и запись списка данных о фильмах.
+
+        Args:
+            search: Объект класса Search.
+
+        Returns:
+            Optional[list[FilmResponse]]: Список объектов модели FilmResponse | None.
+        """
+
+        query = search.to_dict()
+        key = create_key(str(query))
+        films = await self._search_from_cache(key)
+        if not films:
+            try:
+                data = await self._get_search_from_elastic(search)
+                films = [FilmResponse(uuid=row.id, title=row.title, imdb_rating=row.imdb_rating) for row in data]
+            except NotFoundError as ex:  # noqa: F841
+                #  TODO logging
+                return None
+            await self._put_search_to_cache(key, films)
+        return films
 
     async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
         """Получение данных о фильме из Elasticsearch.
@@ -54,34 +93,85 @@ class FilmService:
 
         try:
             doc = await self.elastic.get('movies', film_id)
-        except NotFoundError:
+        except NotFoundError as ex:  # noqa: F841
+            #  TODO logging
             return None
         return Film(**doc['_source'])
 
-    async def _film_from_cache(self, film_id: str) -> Optional[Film]:
+    async def _get_search_from_elastic(self, search: Search) -> Optional[list[Film]]:
+        """
+        Получение списка данных о фильмах из Elasticsearch.
+
+        Args:
+            search: Объект класса Search.
+
+        Returns:
+            Optional[list[Film]]: Список объектов модели Film | None.
+        """
+
+        try:
+            query = search.to_dict()
+            index = search.index[0]
+            data = await self.elastic.search(index=index, body=query)
+            hits = data['hits']['hits']
+            films = [Film(**row('_source')) for row in hits]
+        except NotFoundError as ex:  # noqa: F841
+            #  TODO logging
+            return None
+        return films
+
+    async def _film_from_cache(
+        self,
+        key: str,
+    ) -> Optional[DetailFilmResponse]:
         """Получение данных о фильме из Redis.
 
         Args:
-            film_id: id фильма.
+            key: Ключ.
 
         Returns:
             Optional[Film]: Объект модели Film | None.
         """
 
-        data = await self.redis.get(film_id)
+        data = await self.redis.get(key)
         if not data:
             return None
+        return DetailFilmResponse.parse_raw(data)
 
-        return Film.parse_raw(data)
-
-    async def _put_film_to_cache(self, film: Film) -> None:
-        """Запись данных о фильме в Redis.
+    async def _search_from_cache(self, key: str) -> Optional[list[FilmResponse]]:
+        """Получение списка данных о фильмах из Redis.
 
         Args:
-            film: Объект модели Film.
+            key: ключ.
+
+        Returns:
+            Optional[list[FilmResponse]]:
+                Список объектов модели FilmResponse | None.
+        """
+        data = await self.redis.get(key)
+        if not data:
+            return None
+        return [FilmResponse.parse_raw(film) for film in data]
+
+    async def _put_film_to_cache(self, film: DetailFilmResponse) -> None:
+        """Запись данных о фильме в кеш.
+
+        Args:
+            film: Объект модели DetailFilmResponse.
         """
 
         await self.redis.set(film.id, film.json(), ex=FILM_CACHE_EXPIRE_IN_SECONDS)
+
+    async def _put_search_to_cache(self, key: str, films: list[FilmResponse]) -> None:
+        """Запись данных о фильмах в кеш.
+
+        Args:
+            films: Объект модели Film.
+            key: Ключ.
+        """
+
+        data = orjson.dumps([film.dict() for film in films])
+        await self.redis.set(key, data, ex=FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
 @lru_cache()
@@ -100,3 +190,7 @@ def get_film_service(
     """
 
     return FilmService(redis, elastic)
+
+
+if __name__ == '__main__':
+    ...
