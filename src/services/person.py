1e52099e6805cc1d8129331f8e292fabf848ecb7
Galovach Andrey <a.galovach@yandex.ru>
2022-08-20 04:35:23 +0300

    Перенес SearchMixin

--- src/services/person.py
+++ src/services/person.py
@@ -6,12 +6,11 @@ from aioredis import Redis
 from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 
-from api.v1.utils import SearchMixin
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.film import Film, FilmResponse
 from models.person import DetailPerson, Person
-from services.utils import ElasticMixin, RedisCacheMixin
+from services.utils import ElasticMixin, RedisCacheMixin, SearchMixin
 
 
 class PersonService(SearchMixin, RedisCacheMixin, ElasticMixin):
