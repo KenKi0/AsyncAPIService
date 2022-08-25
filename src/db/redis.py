51449841fe808a287aa2c0be748d31f72e7c8fc8
kenki <ilsafganiev2222@gmail.com>
2022-08-26 00:08:23 +0300

    поправки

--- src/db/redis.py
+++ src/db/redis.py
@@ -2,8 +2,4 @@ import aioredis
 
 from core.config import settings
 
-redis: aioredis.Redis | None = aioredis.from_url(settings.redis.url, max_connections=20, decode_responses=True)
-
-
-async def get_redis() -> aioredis.Redis:
-    return redis
+redis: aioredis.Redis = aioredis.from_url(settings.redis.url, max_connections=20, decode_responses=True)
