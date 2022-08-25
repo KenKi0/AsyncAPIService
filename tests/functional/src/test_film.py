7ed4a359b135062cf3e642f02774d269776ec1aa
Galovach Andrey <a.galovach@yandex.ru>
2022-08-25 19:23:48 +0300

    Добавил тест поиск и пагинация

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,6 +1,6 @@
 import pytest
 from testdata import index_fillings as es_test_data
-from testdata.index_fillings import film_by_id_exepted, film_full_exepted  # noqa: F401
+from testdata.index_fillings import film_by_id_exepted, film_full_exepted, film_pagination_exepted  # noqa: F401
 
 
 @pytest.mark.asyncio
@@ -78,3 +78,28 @@ async def test_full_films(make_get_request_by_search, es_write_data, film_full_e
 
     body = await response.json()
     assert body == sorted(film_full_exepted, key=asc), 'Проверка соответствия данных (ASC).'
+
+
+@pytest.mark.asyncio
+async def test_pagination_films(make_get_request_by_search, es_write_data, film_pagination_exepted):  # noqa: F811
+    """Поиск фильмов на определенной странице."""
+
+    await es_write_data(
+        index='movies',
+        data=es_test_data.movies,
+    )
+    response = await anext(
+        make_get_request_by_search(
+            handler_url='/api/v1/films/',
+            query_data={'sort': '-imdb_rating', 'page[number]': 2, 'page[size]': 10},
+        ),
+    )
+
+    assert response.status == 200
+    body = await response.json()
+    assert len(body) == len(film_pagination_exepted[10:20]), 'Проверка наличия всех фильмов.'
+
+    def key(rating):
+        return -rating['imdb_rating']
+
+    assert body == film_pagination_exepted[10:20], 'Проверка соответствия данных.'
--- tests/functional/testdata/index_fillings.py
+++ tests/functional/testdata/index_fillings.py
@@ -120,3 +120,21 @@ def film_full_exepted():
         }
         for film in movies
     ]
+
+
+@pytest.fixture
+def film_pagination_exepted():
+    def key(rating):
+        return -rating['imdb_rating']
+
+    return sorted(
+        [
+            {
+                'uuid': film.get('id'),
+                'title': film.get('title'),
+                'imdb_rating': film.get('imdb_rating'),
+            }
+            for film in movies
+        ],
+        key=key,
+    )
