ea29924e88dcd788d895d921cbb06f680f51afff
Galovach Andrey <a.galovach@yandex.ru>
2022-08-26 16:50:25 +0300

    start

--- tests/functional/testdata/index_fillings.py
+++ tests/functional/testdata/index_fillings.py
@@ -1,18 +1,18 @@
 import uuid
 
 film_ids_1 = [str(uuid.uuid4()) for _ in range(20)]
-film_ids_2 = [str(uuid.uuid4()) for _ in range(20)]
+film_ids_2 = [str(uuid.uuid4()) for _ in range(30)]
 
 movies = [
     {
         'id': film_id,
         'imdb_rating': 8.5,
         'genre': [
-            {'id': 'g111', 'name': 'Action'},
-            {'id': 'g222', 'name': 'Sci-Fi'},
+            {'id': '6c162475-c7ed-4461-9184-001ef3d9f26e', 'name': 'Action'},
+            {'id': 'fb58fd7f-7afd-447f-b833-e51e45e2a778', 'name': 'Sci-Fi'},
         ],
-        'title': 'The Star',
-        'description': 'New World',
+        'title': 'The Man',
+        'description': 'Man',
         'director': [
             {'id': 'p000', 'name': 'Stan'},
         ],
@@ -33,10 +33,10 @@ movies_2 = [
         'id': film_id,
         'imdb_rating': 8.5,
         'genre': [
-            {'id': 'g333', 'name': 'Drama'},
+            {'id': '120a21cf-9097-479e-904a-13dd7198c1dd', 'name': 'Drama'},
         ],
         'title': 'The Shawshank Redemption',
-        'description': 'Jail break',
+        'description': 'Jail break man',
         'director': [
             {'id': 'p000', 'name': 'Stan'},
         ],
@@ -56,9 +56,9 @@ movies_2 = [
 movies.extend(movies_2)
 
 genres = [
-    {'id': 'g111', 'name': 'Action', 'description': 'Description'},
-    {'id': 'g222', 'name': 'Sci-Fi', 'description': 'Description'},
-    {'id': 'g333', 'name': 'Drama', 'description': 'Description'},
+    {'id': '6c162475-c7ed-4461-9184-001ef3d9f26e', 'name': 'Action', 'description': 'Description'},
+    {'id': 'fb58fd7f-7afd-447f-b833-e51e45e2a778', 'name': 'Sci-Fi', 'description': 'Description'},
+    {'id': '120a21cf-9097-479e-904a-13dd7198c1dd', 'name': 'Drama', 'description': 'Description'},
 ]
 
 persons = [
