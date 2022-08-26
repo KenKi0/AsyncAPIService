65d6b42a3c00ed87d37455e233aad8e081ffaae7
Galovach Andrey <a.galovach@yandex.ru>
2022-08-26 21:03:56 +0300

    Исправил тесты для фильмов.

--- tests/functional/testdata/films.py
+++ tests/functional/testdata/films.py
@@ -10,33 +10,12 @@ def film_by_id_exepted():
         'imdb_rating': 8.5,
         'description': 'Man',
         'genre': [
-            {
-                'uuid': '6c162475-c7ed-4461-9184-001ef3d9f26e',
-                'name': 'Action',
-            },
-            {
-                'uuid': 'fb58fd7f-7afd-447f-b833-e51e45e2a778',
-                'name': 'Sci-Fi',
-            },
+            {'uuid': '6c162475-c7ed-4461-9184-001ef3d9f26e', 'name': 'Action'},
+            {'uuid': 'fb58fd7f-7afd-447f-b833-e51e45e2a778', 'name': 'Sci-Fi'},
         ],
-        'actors': [
-            {
-                'uuid': 'p111',
-                'full_name': 'Ann',
-            },
-        ],
-        'writers': [
-            {
-                'uuid': 'p333',
-                'full_name': 'Ben',
-            },
-        ],
-        'directors': [
-            {
-                'uuid': 'p000',
-                'full_name': 'Stan',
-            },
-        ],
+        'actors': [{'uuid': '26e83050-29ef-4163-a99d-b546cac208f8', 'full_name': 'Ann'}],
+        'writers': [{'uuid': 'e039eedf-4daf-452a-bf92-a0085c68e156', 'full_name': 'Ben'}],
+        'directors': [{'uuid': 'a5a8f573-3cee-4ccc-8a2b-91cb9f55250a', 'full_name': 'Stan'}],
     }
 
 
