43890de065b97d511ffbc88ee166e4a10043313a
Galovach Andrey <a.galovach@yandex.ru>
2022-09-02 08:21:49 +0300

    Добавил backoff для elasticsearch.

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,11 +1,23 @@
 import os
-import time
 
-from elasticsearch import Elasticsearch
+from backoff import backoff
+from elasticsearch import Elasticsearch, TransportError
+
+
+@backoff()
+def check_connection(host: str = os.environ.get('ELASTIC_HOST')) -> bool:
+    """
+    Реализация отказоустойчивости, проверка связи с сервером Elasticsearch.
+    :param host: URL.
+    :return: bool.
+    """
+    es_client = Elasticsearch(host)
+    if not es_client.ping():
+        raise TransportError('Нет связи с сервером Elasticsearch')
+    return es_client.ping()
+
 
 if __name__ == '__main__':
-    es_client = Elasticsearch(hosts=os.environ.get('ELASTIC_HOST'))
     while True:
-        if es_client.ping():
+        if check_connection():
             break
-        time.sleep(1)
